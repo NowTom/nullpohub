2e1629597ae6dc37a45096554a4815015701f699
sinsoku <sinsoku.listy@gmail.com>
2013-05-25 20:40:23 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nullpohub::Application.config.secret_token = '8b5ae46c4de0aefc5dd4f65a4c7b24184e069461b39d3b5823c442294fb675a73cfca62d20f0409dfbcf53790505336fd28717acabdad5b8493193be20e5d5e6'
