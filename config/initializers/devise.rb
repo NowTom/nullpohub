198acd25e14d7521d69037972a2430450419979b
sinsoku <sinsoku.listy@gmail.com>
2013-05-26 00:36:58 +0900

    githubのscopeは不要なので削除

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -219,7 +219,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, Settings.github.key, Settings.github.token, scope: 'user,public_repo'
+  config.omniauth :github, Settings.github.key, Settings.github.token
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
